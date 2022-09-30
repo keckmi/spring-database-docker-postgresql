e9278fb9cb9ff9835587bf2a8a1b39c5f8d601de
michael <michael-s-keck@web.de>
2022-09-30 09:48:49 +0200

    Initial commit

---
+++ src/main/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatabasedockerpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseDockerPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseDockerPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatabasedockerpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseDockerPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
