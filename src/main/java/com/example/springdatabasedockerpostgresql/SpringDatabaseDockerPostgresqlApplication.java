19ac63ba520d7320325d3239d9efa2be0e84735d
michael <michael-s-keck@web.de>
2022-09-30 10:26:12 +0200

    main class add two objects in database

--- src/main/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplication.java
+++ src/main/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplication.java
@@ -2,12 +2,27 @@ package com.example.springdatabasedockerpostgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDatabaseDockerPostgresqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDatabaseDockerPostgresqlApplication.class, args);
+
+		ConfigurableApplicationContext applicationContext =
+				SpringApplication.run(SpringDatabaseDockerPostgresqlApplication.class, args);
+
+		City berlin = new City();
+		berlin.setName("Berlin");
+		berlin.setCapital(true);
+
+		City duesseldorf = new City();
+		duesseldorf.setCapital(false);
+		duesseldorf.setName("Duesseldorf");
+
+		CityRepository cityRepository = applicationContext.getBean(CityRepository.class);
+		cityRepository.save(berlin);
+		cityRepository.save(duesseldorf);
 	}
 
 }
