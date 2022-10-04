449a99a9efcaa3f31cd46b4992cadfbb5c5ad544
michael <michael-s-keck@web.de>
2022-10-04 09:19:03 +0200

    JPA Query Language implementations

--- src/main/java/com/example/springdatabasedockerpostgresql/CityRepository.java
+++ src/main/java/com/example/springdatabasedockerpostgresql/CityRepository.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository //Damit Spring sraus Kontext erzeugt, den wir in Bean reintun oder autowiren können
 public interface CityRepository extends CrudRepository<City, Long> {
+
+    //JPA Query Language
+    //SELECT 1 From cities WHERE name = x
+    City findByName(String name);
 }
--- src/main/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplication.java
+++ src/main/java/com/example/springdatabasedockerpostgresql/SpringDatabaseDockerPostgresqlApplication.java
@@ -1,11 +1,39 @@
 package com.example.springdatabasedockerpostgresql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringDatabaseDockerPostgresqlApplication {
+
+	//zu JPA Query Language
+	//Repository initiieren in eine beliebeige Bean
+	@RestController
+	public class CityController {
+		@Autowired
+		CityRepository cityRepository;
+
+		//nach Programmausführung einsehbar unter http://localhost:8080/berlin
+		@GetMapping("/berlin")
+		public City berlin() {
+			return cityRepository.findByName("Berlin");
+		}
+		//nach Programmausführung einsehbar unter http://localhost:8080/duesseldorf
+		@GetMapping("/duesseldorf")
+		public City duesseldorf() {
+			return cityRepository.findByName("Duesseldorf");
+		}
+		//nach Programmausführung einsehbar unter http://localhost:8080/winnenden
+		@GetMapping("/winnenden")
+		public City winnenden() {
+			return cityRepository.findByName("Winnenden");
+		}
+
+	}
 
 	public static void main(String[] args) {
 
